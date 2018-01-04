33ec0b57328ed0a00227e890f2a93bb873344941
El7aby <thegostkasper@hotmail.com>
2018-01-04 16:41:21 +0200

    commit changes

---
+++ talktive/src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import { PetService } from './services/app.service.pet';
+import { AuthenticationService } from './services/app.authentication';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  loginStatus: string;
+
+  constructor() {
+    this.loginStatus = (localStorage.getItem('token')) ? 'Welcome' : 'login';
+  }
+  ngOnInit() {
+  }
+}
